520ec51fdf098bd529ea7a385f93da60b3a9ff41
Johannes Haapanen <72503684+joakha@users.noreply.github.com>
2024-02-11 14:25:49 +0200

    First Commit

---
+++ src/main/java/bookstore/web/BookController.java
@@ -0,0 +1,17 @@
+package bookstore.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class BookController {
+
+    @GetMapping("/index")
+    public String showIndex(Model model) {
+
+        return "index";
+        
+    }
+
+}
