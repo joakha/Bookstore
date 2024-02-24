bbf5b3e8551b27ddce4b7966780ab6d1a9003c93
joakha <72503684+joakha@users.noreply.github.com>
2024-02-24 14:54:41 +0200

    implement booklist page

--- src/main/java/bookstore/web/BookController.java
+++ src/main/java/bookstore/web/BookController.java
@@ -1,17 +1,31 @@
 package bookstore.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import bookstore.domain.BookRepository;
 
 @Controller
 public class BookController {
+
+    @Autowired
+    private BookRepository repository;
 
     @GetMapping("/index")
     public String showIndex(Model model) {
 
         return "index";
         
+    }
+
+    @GetMapping("/booklist")
+    public String bookList(Model model) {
+
+        model.addAttribute("books", repository.findAll());
+
+        return "booklist";
+
     }
 
 }
