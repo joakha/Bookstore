6f8dde3872c1016dcc5e8721781ac58ed13063ec
joakha <72503684+joakha@users.noreply.github.com>
2024-02-25 11:48:20 +0200

    implement save and add functionality

--- src/main/java/bookstore/web/BookController.java
+++ src/main/java/bookstore/web/BookController.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import bookstore.domain.Book;
 import bookstore.domain.BookRepository;
 
 @Controller
@@ -27,5 +30,30 @@ public class BookController {
         return "booklist";
 
     }
+
+    @GetMapping("/addbook")
+    public String addBook(Model model) {
+
+        model.addAttribute("book", new Book());
+
+        return "bookform";
+
+    }
+
+    @PostMapping("/savebook")
+    public String saveBook(Book book) {
+
+        repository.save(book);
+
+        return "redirect:/booklist";
+
+    }
+
+	@GetMapping("/deletebook/{id}")
+	public String deleteBook(@PathVariable("id") Long bookId) {
+		repository.deleteById(bookId);
+		return "redirect:/booklist";
+
+	}
 
 }
